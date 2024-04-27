a7308436ac527a6d1678f6c3caa4612bebb94d34
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-04-27 11:54:08 +0500

    Qr code generator

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
